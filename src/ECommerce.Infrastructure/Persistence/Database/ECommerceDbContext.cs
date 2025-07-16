af62c14c7ebd09b952af88b72393b7120d991566
OneWay2Go <uchqunov.m@yandex.com>
2025-07-16 05:55:43 +0500

    Database is added

---
+++ src/ECommerce.Infrastructure/Persistence/Database/ECommerceDbContext.cs
@@ -0,0 +1,41 @@
+﻿using ECommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Infrastructure.Persistence.Database;
+
+public class ECommerceDbContext : DbContext
+{
+    public ECommerceDbContext(DbContextOptions<ECommerceDbContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+
+    public DbSet<Role> Roles { get; set; }
+
+    public DbSet<Permission> Permissions { get; set; }
+
+    public DbSet<UserRole> UserRoles { get; set; }
+
+    public DbSet<RolePermission> RolePermissions { get; set; }
+
+    public DbSet<Cart> Carts { get; set; }
+
+    public DbSet<CartItem> CartItems { get; set; }
+
+    public DbSet<Category> Categories { get; set; }
+
+    public DbSet<Coupon> Coupons { get; set; }
+
+    public DbSet<Order> Orders { get; set; }
+
+    public DbSet<OrderItem> OrderItems { get; set; }
+
+    public DbSet<Payment> Payments { get; set; }
+
+    public DbSet<Product> Products { get; set; }
+
+    public DbSet<Review> Reviews { get; set; }
+
+    public DbSet<ShippingAddress> ShippingAddresses { get; set; }
+
+    public DbSet<WishList> WishLists { get; set; }
+}
